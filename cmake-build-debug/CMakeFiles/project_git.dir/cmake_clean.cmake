ac60e2836ea0d7495362e17fd3a41469d1294022
LAPTOP-C5LG2SPO\wiete <236007@edu.p.lodz.pl>
2021-02-17 08:37:23 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/project_git.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/project_git.dir/main.cpp.obj"
+  "libproject_git.dll.a"
+  "project_git.exe"
+  "project_git.exe.manifest"
+  "project_git.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/project_git.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/project_git.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for project_git.
+# This may be replaced when dependencies are built.
